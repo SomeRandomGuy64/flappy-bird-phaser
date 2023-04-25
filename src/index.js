5d9ede013242341f7954c76d1db033ea0a7edef0
somerandomguy64 <faizan163123@gmail.com>
2023-04-25 20:16:30 +0100

    setting up

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-
 import Phaser from "phaser";
 
 const config = {
+  // WebGL is the renderer automatically used
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
+    //Arcade physics plugin
+    default: 'arcade'
   },
   scene: {
     preload: preload,
-    create: create
+    create: create,
   }
-};
+}
+
+function preload() {
+  debugger;
+}
+
+function create() {
+  debugger;
+}
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
